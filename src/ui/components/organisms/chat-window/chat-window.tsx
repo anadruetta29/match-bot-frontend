c9dd4eca22f09729c21916b65f7fa3de7f9eb593
anadruetta29 <anadruetta2023@gmail.com>
2026-01-09 02:11:33 -0300

    add: chat window component

---
+++ src/ui/components/organisms/chat-window/chat-window.tsx
@@ -0,0 +1,34 @@
+import type { Message } from "../../../../domain";
+import BubbleMessage from "../../atoms/bubble-message/bubble-message";
+import SendInput from "../../molecules/send-input/send-input";
+import style from "./style.module.css";
+
+type Props = {
+    messages: Message[];
+    inputValue: string;
+    onChangeInput: (value: string) => void;
+    onSend: () => void;
+};
+
+export default function ChatWindow({
+  messages,
+  inputValue,
+  onChangeInput,
+  onSend
+}: Props) {
+  return (
+    <div className={style.container}>
+      <div className={style.messages}>
+        {messages.map((message, index) => (
+          <BubbleMessage key={index} message={message} />
+        ))}
+      </div>
+
+      <SendInput
+        value={inputValue}
+        onChange={onChangeInput}
+        onClickOnSend={onSend}
+      />
+    </div>
+  );
+}
